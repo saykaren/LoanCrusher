239991416e4f84d99dcdc96c13e6a79382e6c9d2
Karen Garcia <kgarcia@i3screen.com>
2020-04-20 20:44:20 -0600

    Updates

---
+++ precache-manifest.6f0b8e4e529f5d61b99da23a0a78999c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488b0c7c1e59ae5e31c9b7720f03b480",
+    "url": "/LoanCrusher/index.html"
+  },
+  {
+    "revision": "bb26dff8df333e8be3f4",
+    "url": "/LoanCrusher/static/css/main.a603c953.chunk.css"
+  },
+  {
+    "revision": "0a09ed9988bbe3f9ff51",
+    "url": "/LoanCrusher/static/js/2.16523648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LoanCrusher/static/js/2.16523648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb26dff8df333e8be3f4",
+    "url": "/LoanCrusher/static/js/main.5bf420d9.chunk.js"
+  },
+  {
+    "revision": "2109cdde0cf56bcdbc97",
+    "url": "/LoanCrusher/static/js/runtime-main.b30f6dbf.js"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/LoanCrusher/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
